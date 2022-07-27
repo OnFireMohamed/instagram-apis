4bd9fa387a6b9ad6224a6d3a2537051f8acee042
Mohammed Al-Jahwari <88824957+majhcc@users.noreply.github.com>
2022-07-27 12:37:46 +0400

    add new func FromURLtoMediaid

--- src/mediaAPIs.js
+++ src/mediaAPIs.js
@@ -1,6 +1,12 @@
 const axios = require("axios");
 const newInboxAPIs = require("./newInboxAPIs.js");
+var bigint = require( 'big-integer' )
 
+var lower = 'abcdefghijklmnopqrstuvwxyz';
+var upper = lower.toUpperCase();
+var numbers = '0123456789'
+var ig_alphabet =  upper + lower + numbers + '-_'
+var bigint_alphabet = numbers + lower
 module.exports = class mediaAPIs extends newInboxAPIs {
     initialize(headers) {
         this.headers = headers;
@@ -15,6 +21,26 @@ module.exports = class mediaAPIs extends newInboxAPIs {
             else throw new Error("Unvalid url")
         } catch ({response: {data}}) {
             throw new Error(data);
+        }
+    }
+    // https://stackoverflow.com/a/68497086/14779443
+    fromShortcode( shortcode )
+    {
+        var o = shortcode.replace( /\S/g, m =>
+        {
+        var c = ig_alphabet.indexOf( m )
+        var b = bigint_alphabet.charAt( c ) 
+        return ( b != "" ) ? b : `<${c}>`
+        } ) 
+        return bigint( o, 64 ).toString( 10 )
+    }
+    async getMediaIdFromPostUrl(url) {
+        const re = new RegExp("https://www.instagram.com/.*?/(.*?)/");
+        let shortcode = url.match(re);
+        if (shortcode) {
+            let id = this.fromShortcode(shortcode[1]);
+            let { data } = await axios.get(`https://i.instagram.com/api/v1/media/${id}/info/`, {headers: this.headers})
+            return data.items[0].id;
         }
     }
     async getMediaInfoFromMediaId(media_id) {
